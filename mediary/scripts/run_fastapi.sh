30b4cc6ceecf4ac4c60f93893927edfb5029325c
PaszaVonPomiot <schedar@op.pl>
2024-07-16 02:16:09 +0200

    upgrade fastapi dockerfile, add docker compose, add .env

--- mediary/scripts/run_fastapi.sh
+++ mediary/scripts/run_fastapi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Environment: $ENV"
+echo "Environment: $APP_ENV"
 echo "Launching FastAPI"
 
-if [ "$ENV" == "LOCAL" ]; then
+if [ "$APP_ENV" == "LOCAL" ]; then
   exec uvicorn main:app --app-dir app/ \
                         --host 0.0.0.0 \
                         --port 8000 \
