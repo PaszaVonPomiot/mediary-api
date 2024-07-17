5e1f55580f043fb121ebcf4eb6453a073eff0948
PaszaVonPomiot <schedar@op.pl>
2024-07-17 23:41:57 +0200

    update entrypoint

--- mediary/scripts/run_fastapi.sh
+++ mediary/scripts/run_fastapi.sh
@@ -1,16 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+APP_DIR="app"
+APP_HOST="0.0.0.0"
+APP_PORT=8000
+
+echo "Starting FastAPI..."
 echo "Environment: $APP_ENV"
-echo "Launching FastAPI"
 
 if [ "$APP_ENV" == "LOCAL" ]; then
-  exec uvicorn main:app --app-dir app/ \
-                        --host 0.0.0.0 \
-                        --port 8000 \
+  exec uvicorn main:app --app-dir $APP_DIR \
+                        --host $APP_HOST \
+                        --port $APP_PORT \
                         --reload
 else
-  exec uvicorn main:app --app-dir app/ \
-                        --host 0.0.0.0 \
-                        --port 8000 \
+  exec uvicorn main:app --app-dir $APP_DIR \
+                        --host $APP_HOST \
+                        --port $APP_PORT \
                         --header server:HIDDEN;
 fi
